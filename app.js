be96330ab2c17374d4250c2d26e9a827c7cd1d40
robyng <robyngraham01@gmail.com>
2021-11-11 10:57:56 -0800

    add generatepage function to page-template.js file

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const generatePage = require('./src/page-template.js');
 
-  console.log('================');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw new err;
+  console.log('Portfolio is complete! Checkout index.html to see the output!')
+});
--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-const fs = require('fs');
+const inquirer = require("inquirer");
 
-fs.writeFile('README2.md', '# Description', (error, data) => {
-    if(error) {console.log("there was error", err)}
-    console.log("succes")
+inquirer
+.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "what is your name"
+    }
+])
+
+.then((answers) => {
+    console.log(answers)
 })
 
-fs.readFile('data.txt', 'utf-8', (error, data) => {
-    if (error) throw error;
-    console.log("here is what's in data text file", data)
+.catch((error) => {
+    console.log(answers.firstName)
 })
 
-const fs = requre('fs';
-console.log(process))
+.catch((error) => {
+    console.log(error)
+})
+
+// const fs = require('fs');
+
+// fs.writeFile('README2.md', '# Description', (error, data) => {
+//     if(error) {console.log("there was error", err)}
+//     console.log("succes")
+// })
+
+// fs.readFile('data.txt', 'utf-8', (error, data) => {
+//     if (error) throw error;
+//     console.log("here is what's in data text file", data)
+// })
+
+// const fs = requre('fs';
+// console.log(process))
