bd5c1b715ca4994df039286515e9bd17371bc5de
robyng <robyngraham01@gmail.com>
2021-11-15 09:12:00 -0800

    add when method for about section

--- app.js
+++ app.js
@@ -27,12 +27,24 @@ const promptUser = () => {
           return false;
         }
       }
-      
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to add information about yourself for an About section?',
+      default: true
     },
     {
       type: 'input',
       name: 'about',
-      message: 'Tell us about yourself'
+      message: 'Tell us about yourself',
+      when: ({confirmAbout}) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
   ]);
 };
